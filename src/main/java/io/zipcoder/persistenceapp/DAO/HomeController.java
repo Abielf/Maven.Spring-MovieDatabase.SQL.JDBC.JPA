7551752526d4a2adf0b22e2ce10ce5f73a889c85
Abiel Figueroa <Abe@zipcodes-MacBook-Pro-2.local>
2021-04-27 18:31:19 -0400

    woo more done

--- src/main/java/io/zipcoder/persistenceapp/DAO/HomeController.java
+++ src/main/java/io/zipcoder/persistenceapp/DAO/HomeController.java
@@ -1,4 +1,45 @@
 package io.zipcoder.persistenceapp.DAO;
 
+
+import io.zipcoder.persistenceapp.Home;
+import io.zipcoder.persistenceapp.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/homes")
+@RestController
 public class HomeController {
+    HomeService homeService;
+
+    @Autowired
+    public HomeController(HomeService h){
+        homeService=h;
+    }
+
+    @GetMapping
+    public List<Home> neighborhood(){return homeService.list();}
+
+    @GetMapping(path="byid/{id}")
+    public Home getHomeById(@PathVariable int id){return homeService.get(id).get();}
+
+    @GetMapping(path="byphone/{num}")
+    public Home getHomeByHumeNumber(@PathVariable String num){return homeService.getByHomeNumber(num).get();}
+
+    //@GetMapping(path="/byaddress/{address}")
+    //public Home getHomeByAddress(@PathVariable String address){return homeService.getByAddress(address).get();}
+    @GetMapping(path="bypersonid/{id}")
+    public Home getHomeByPersonId(@PathVariable int id){return homeService.getByPersonId(id).get();}
+
+
+    @PostMapping
+    public void newHome(@RequestBody Home h){ homeService.create(h);}
+
+    @PutMapping(path="{id}")
+    public void updateHome(@RequestBody Home h, @PathVariable int id){homeService.update(h,id);}
+
+    @DeleteMapping(path="{id}")
+    public void deleteHouseById(@PathVariable int id){homeService.delete(id);}
+
 }
--- src/main/java/io/zipcoder/persistenceapp/HomeService.java
+++ src/main/java/io/zipcoder/persistenceapp/HomeService.java
@@ -4,12 +4,15 @@ import io.zipcoder.persistenceapp.DAO.DAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class HomeService implements DAO<Home> {
     private static final Logger log = LoggerFactory.getLogger(HomeService.class);
 
@@ -33,23 +36,71 @@ public class HomeService implements DAO<Home> {
         return jdbcTemplate.query(sql,rowMapper);
     }
 
+    public Optional<Home> get(int id) {
+        String sql="SELECT ID, ADDRESS, HOMENUMBER FROM HOME WHERE ID=?";
+        Home home = null;
+        try{
+            home=jdbcTemplate.queryForObject(sql, new Object[]{id}, rowMapper);
+        }catch(DataAccessException e){log.info("Home not found");}
+        return Optional.ofNullable(home);
+    }
+
+    public Optional<Home> getByHomeNumber(String num) {
+        String sql="SELECT ID, ADDRESS, HOMENUMBER FROM HOME WHERE HOMENUMBER=?";
+        Home home = null;
+        try{
+            home=jdbcTemplate.queryForObject(sql, new Object[]{num}, rowMapper);
+        }catch(DataAccessException e){log.info("Person not found");}
+        return Optional.ofNullable(home);
+    }
+
+    public Optional<Home> getByAddress(String address) {
+        String sql="SELECT ID, ADDRESS, HOMENUMBER FROM HOME WHERE HOMENUMBER=?";
+        Home home = null;
+        try{
+            home=jdbcTemplate.queryForObject(sql, new Object[]{address}, rowMapper);
+        }catch(DataAccessException e){log.info("Person not found");}
+        return Optional.ofNullable(home);
+    }
+
+    public Optional<Home> getByPersonId(int id) {
+        String sql="SELECT HOME.ID, HOME.ADDRESS, HOME.HOMENUMBER FROM HOME INNER JOIN PERSON ON PERSON.HOME_ID=HOME.ID WHERE PERSON.ID=?";
+        Home home = null;
+        try{
+            home=jdbcTemplate.queryForObject(sql, new Object[]{id}, rowMapper);
+        }catch(DataAccessException e){log.info("Person not found");}
+        return Optional.ofNullable(home);
+    }
+
+
+
     @Override
     public void create(Home home) {
-
+        String sql="INSERT INTO HOME (ADDRESS,HOMENUMBER) VALUES (?,?)";
+        int insert= jdbcTemplate.update(sql, home.getAddress(), home.getHomeNumber() );
+                if(insert==1){log.info("Home with address:"+home.getAddress()+" Added to database.");}
     }
 
-    @Override
-    public Optional<Home> get(int id) {
-        return Optional.empty();
-    }
 
     @Override
     public void update(Home home, int id) {
-
+        String sql="UPDATE HOME SET ADDRESS=?, HOMENUMBER=? WHERE ID=?";
+        int update= jdbcTemplate.update(sql, home.getAddress(),home.getHomeNumber() ,id);
+        if(update==1){log.info("Home Updated");}
     }
 
     @Override
     public void delete(int id) {
-
+        jdbcTemplate.update("DELETE FROM HOME WHERE ID=?",id);
     }
 }
+/*
+Add a person to a home
+Remove a list of homes from the database
+Generate a list of people that live in a home
+
+
+Remove a home from the database
+Find a home by address
+Update an existing 'Home' in the database
+*/
